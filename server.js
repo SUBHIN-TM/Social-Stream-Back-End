0f9e1c25f3611f8585b507e15e48aa0cd7fd6b3f
SUBHIN-TM <subhintm2018@gmail.com>
2024-04-02 17:14:54 +0530

    cors

--- server.js
+++ server.js
@@ -9,17 +9,18 @@ import  {connect} from "./Utilities/mongodb.js";
 const app=express()
 const port=3000
 var allowedOrigins = ['http://localhost:5173', 'http://anotherexample.com'];
-
+app.use(cors())
 app.use(express.json())
-app.use(cors({
-    origin:(origin,callback)=>{
-        if(!origin || allowedOrigins.includes(origin)){
-            callback(null,true)
-        }else{
-            callback(new Error('your oringin is not allowed by cors'))
-        }
-    }
-}))
+
+// app.use(cors({
+//     origin:(origin,callback)=>{
+//         if(!origin || allowedOrigins.includes(origin)){
+//             callback(null,true)
+//         }else{
+//             callback(new Error('your oringin is not allowed by cors'))
+//         }
+//     }
+// }))
 
 
 app.use('/',user)
