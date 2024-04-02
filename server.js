c71a4fe9626bb65ba391819622f96e1e7feec855
SUBHIN-TM <subhintm2018@gmail.com>
2024-04-02 20:14:33 +0530

    recommit

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 import express from "express"
 import cors from "cors"
 import user from "./routers/user.js"
+import  {connect} from "./Utilities/mongodb.js";
+
 
 
 
 const app=express()
 const port=3000
-
-app.use(express.json())
+var allowedOrigins = ['http://localhost:5173', 'http://anotherexample.com'];
 app.use(cors())
+app.use(express.json())
+
+// app.use(cors({
+//     origin:(origin,callback)=>{
+//         if(!origin || allowedOrigins.includes(origin)){
+//             callback(null,true)
+//         }else{
+//             callback(new Error('your oringin is not allowed by cors'))
+//         }
+//     }
+// }))
+
+
 app.use('/',user)
 
 app.listen(port,()=>{
     console.log("Server is running on 3000");
+    connect()
 })
