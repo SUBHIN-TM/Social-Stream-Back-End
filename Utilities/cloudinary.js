9f0e872e5bdd27f7b3102b0576296e8786066dd4
SUBHIN-TM <subhintm2018@gmail.com>
2024-04-02 17:43:28 +0530

    upload gitignoer

--- Utilities/cloudinary.js
+++ Utilities/cloudinary.js
@@ -12,11 +12,6 @@ cloudinary.config({
   api_secret: process.env.API_SECRET 
 });
 
-// cloudinary.config({ 
-//     cloud_name: 'dsgctlkzt', 
-//     api_key: '764986656521813', 
-//     api_secret: 'V0EFNneZElocCS6uUnX8-22khew' 
-//   });
 
 export default cloudinary;
 
